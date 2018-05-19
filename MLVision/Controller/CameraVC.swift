efa929b366941c072a9951e3e77070e6d1772ffc
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-05-18 23:21:55 -0700

    Builded the UI & custom classes for UI

--- MLVision/Controller/CameraVC.swift
+++ MLVision/Controller/CameraVC.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class CameraVC: UIViewController {
 
+    @IBOutlet weak var captureImageView: RoundedShadowImageView!
+    @IBOutlet weak var flashButton: RoundedShadowButton!
+    @IBOutlet weak var identificationLbl: UILabel!
+    @IBOutlet weak var confidenceLbl: UILabel!
+    @IBOutlet weak var cameraView: UIView!
+    
+    @IBOutlet weak var roundedLblView: RoundedShadowView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
---
+++ MLVision/View/RoundedShadowImageView.swift
@@ -0,0 +1,29 @@
+//
+//  RoundedShadowImageView.swift
+//  MLVision
+//
+//  Created by Nathaniel Burciaga on 5/18/18.
+//  Copyright © 2018 Nathaniel Burciaga. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowImageView: UIImageView {
+
+    
+    override func awakeFromNib() {
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 15
+        self.layer.opacity = 0.75
+        self.layer.cornerRadius = self.frame.height / 2
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
